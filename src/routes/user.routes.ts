f7c71f0bdf35d44489936e4b70ed4b721561b45b
pitakarn99 <chatchawant03@gmail.com>
2026-04-09 17:08:44 +0700

    fix route upload

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,17 +2,17 @@ import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
 import { authenticateToken } from "../middleware/auth.middleware.js";
 import multer from 'multer';
-const upload = multer({ dest: 'storage/temp/' });
+// const upload = multer({ dest: 'storage/temp/' });
 
 const router = Router();
 
 router.post("/SignUp", userController.SignUp);
 router.post("/Login", userController.Login);
 router.get("/CheckAlreadyExistsEmail", userController.CheckAlreadyExistsEmail);
-router.post("/ApplySeller", authenticateToken, upload.fields([
-  { name: 'IdCardImage', maxCount: 1 },
-  { name: 'SelfieImage', maxCount: 1 }
-]), userController.ApplySeller);
+// router.post("/ApplySeller", authenticateToken, upload.fields([
+//   { name: 'IdCardImage', maxCount: 1 },
+//   { name: 'SelfieImage', maxCount: 1 }
+// ]), userController.ApplySeller);
 router.post("/GetJwt", authenticateToken, (req, res) => {
   res.json({ message: "This is a protected route", user: (req as any).user });
 });
